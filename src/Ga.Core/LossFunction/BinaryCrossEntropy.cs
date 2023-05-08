96dd2cb1d54e007c89f8fb7945fadf7e33e8b09b
”Daniil <das76@tpu.ru>
2023-05-08 22:14:09 +0700

    Progress

--- src/Ga.Core/LossFunction/BinaryCrossEntropy.cs
+++ src/Ga.Core/LossFunction/BinaryCrossEntropy.cs
@@ -4,9 +4,11 @@ public class BinaryCrossEntropy : ILossFunction
 {
     public double CalculateError(IList<double> actual, IList<double> expected)
     {
-        var probabilities = GetProbabilities(actual);
+        var probabilities = GetProbabilities(actual).ToList();
 
-        var result = -(1 * Math.Log(probabilities.First()));
+        var y = Convert.ToInt32(expected.First()) == 1 ? 1 : 0;
+
+        var result = -(y * Math.Log(probabilities[0]) + (1-y)*Math.Log(1-probabilities[0]));
 
         return result;
     }
